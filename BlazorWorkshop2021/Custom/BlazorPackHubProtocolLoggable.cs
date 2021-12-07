16c856c0de381d65e1e34f17d6e823b167ab17b3
Rodion Mostovoy <36400912+rodion-m@users.noreply.github.com>
2021-12-08 01:41:45 +0300

    Initial commit

---
+++ BlazorWorkshop2021/Custom/BlazorPackHubProtocolLoggable.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Buffers;
+using System.Text;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace BlazorWorkshop2021.Custom
+{
+    /// <summary>
+    /// Decorator (proxy) for an original <see cref="Microsoft.AspNetCore.Components.Server.BlazorPack.BlazorPackHubProtocol"/>
+    /// </summary>
+    internal sealed class BlazorPackHubProtocolLoggable : IHubProtocol
+    {
+        private readonly IHubProtocol _base;
+        private readonly ILogger<BlazorPackHubProtocolLoggable> _logger;
+
+        public BlazorPackHubProtocolLoggable(IHubProtocol @base, ILogger<BlazorPackHubProtocolLoggable> logger)
+        {
+            _base = @base;
+            _logger = logger;
+        }
+
+        public bool TryParseMessage(ref ReadOnlySequence<byte> input, IInvocationBinder binder, out HubMessage? message)
+        {
+            var str = Encoding.UTF8.GetString(input.ToArray());
+            var result = _base.TryParseMessage(ref input, binder, out message);
+            _logger.LogWarning("IN: {@Message}", message);
+            return result;
+        }
+
+        public void WriteMessage(HubMessage message, IBufferWriter<byte> output)
+        {
+            _logger.LogInformation("OUT: {@Message}", message);
+            _base.WriteMessage(message, output);
+        }
+
+        public ReadOnlyMemory<byte> GetMessageBytes(HubMessage message)
+            => _base.GetMessageBytes(message);
+
+        public bool IsVersionSupported(int version)
+            => _base.IsVersionSupported(version);
+
+        public string Name => _base.Name;
+        public int Version => _base.Version;
+        public TransferFormat TransferFormat => _base.TransferFormat;
+    }
+}
